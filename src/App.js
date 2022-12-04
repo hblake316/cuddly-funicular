47745054eb8840339988e3d5b10bed71ec8bbefc
Heather Blake <hblake@smatechnologies.com>
2022-12-04 14:33:14 -0600

    add functionality and cleanup files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
 
 function App() {
+  const [cards, setCards] = useState(null)
+
+  useEffect(() => {
+    async function drawCard() {
+      const response = await fetch(
+        `https://deckofcardsapi.com/api/deck/new/draw/?count=1`
+      )
+      const data = await response.json()
+      const img = data.cards.map((card) => card.image)
+      setCards(img[0])
+      console.log(data)
+    }
+    drawCard()
+  }, [])
+
+  async function drawCard() {
+    const response = await fetch(
+      `https://deckofcardsapi.com/api/deck/new/draw/?count=1`
+    )
+    const data = await response.json()
+    const img = data.cards.map((card) => card.image)
+    setCards(img[0])
+    console.log(data)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <p>
+        <code>Card Guessing Game</code>
+      </p>
+      <img
+        src={cards}
+        className='card'
+        alt='card'
+        onClick={() => {
+          drawCard()
+        }}
+      />
+      <p>
+        <code>Click the card to draw another</code>
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
